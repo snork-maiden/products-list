33bb40a0375fd909a2741f1e0b4e22c901c64fb8
snork-maiden <pollykoldysheva@gmail.com>
2024-03-07 17:36:42 +0300

    Add Pagination, remove unused

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,16 +1,28 @@
+/* eslint-disable react/prop-types */
 
+import "./Pagination.module.css";
 
-import './Pagination.module.css'
-
-function Pagination({page, onChange}) {
-
+function Pagination({ page, onChange }) {
   return (
     <div className="wrapper">
-      <button type="button" className="paginate" onClick={() => onChange(page-1)}> Назад </button>
+      <button
+        type="button"
+        className="paginate"
+        onClick={() => onChange(page - 1)}
+        disabled={page < 2}
+      >
+        Назад
+      </button>
       {page}
-      <button type="button" className="paginate" onClick={() => onChange(page+1)}>Вперед</button>
+      <button
+        type="button"
+        className="paginate"
+        onClick={() => onChange(page + 1)}
+      >
+        Вперед
+      </button>
     </div>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
